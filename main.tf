64d0b88e4234565382de387af642bf55bbf3d415
dinu30 <dinu30@gmail.com>
2023-04-16 14:55:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  region      = "ap-south-1"
+  region     = "ap-south-1"
 }
-resource "aws_instance" "Amazon_Linux_2023"
+resource "aws_instance" "Amazon_Linux_2023" {
   ami           = "ami-07d3a50bd29811cd1"
   instance_type = "t2.micro"
   key_name      = "New_MC"
   tags = {
-    Name = "Git_Learning_Server"
+    Name = "Learning_Server"
   }
+} 
 
